bf1b67cdb602aead3e7040a6c514958b572458f8
keishimizu26629 <keisukeshimizu.inoworl@gmail.com>
2025-11-20 12:36:19 +0900

    Update toggle-subdisplay formula to v0.1.0
    - Update SHA256 hash for v0.1.0 release
    - Formula now points to working GitHub release
    - Ready for Homebrew installation testing

--- toggle-subdisplay.rb
+++ toggle-subdisplay.rb
@@ -2,7 +2,7 @@ class ToggleSubdisplay < Formula
   desc "CLI to toggle Mac subdisplay settings between mirror and extended modes"
   homepage "https://github.com/keishimizu26629/toggle-subdisplay"
   url "https://github.com/keishimizu26629/toggle-subdisplay/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_THIS_WITH_SHA"
+  sha256 "ff06baa9a0bfb036294ee698e5c67032a2d79b4c48878f54b7137c29f0cab587"
   license "MIT"
 
   depends_on xcode: ["12.0", :build]
